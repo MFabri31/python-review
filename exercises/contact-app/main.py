60167c5313561f8a941292e75fc511293b5be1d6
mfabri31 <fabriciomedina9531@gmail.com>
2026-07-13 11:12:18 -0300

    add contact management functionality

---
+++ exercises/contact-app/main.py
@@ -0,0 +1,76 @@
+import os
+
+os.system('cls')
+
+
+
+contacts = [
+    {
+       "id":1, 
+       "name":"contact example", 
+       "phone":"123245678", 
+    }
+]
+
+
+def add_contact():
+    print('===== Agregar contacto =====')
+    contact_name = input('Nombre: >')
+    contact_phone = input('Teléfono: >')
+    
+    new_contact = {
+        "id": len(contacts) + 1,
+        "name": contact_name,
+        "phone": contact_phone   
+    }
+    
+    contacts.append(new_contact)
+
+def list_contacts():
+    print('Listar contactos: ')
+
+def search_contact():
+    print('Buscar contacto: ')
+    pass
+
+def delete_contact():
+    print('Eliminar contacto: ')
+    pass
+
+def exit():
+    print('Saliendo... ')
+    pass
+
+
+
+
+
+while True:
+    
+    print('======================= ')
+    print('====== Bienvenido ===== ')
+    print('======================= ')
+    print(
+    "[1] Agregar contacto\n"
+    "[2] Listar contactos\n"
+    "[3] Buscar contacto\n"
+    "[4] Eliminar contacto\n"
+    "[5] Salir"
+    )
+    option = int(input('Ingrese una opción: >'))
+    
+    os.system('cls')
+    
+    if option == 1:
+        add_contact()
+    if option == 2:
+        list_contacts()
+    if option == 3:
+        search_contact()
+    if option == 4:
+        delete_contact()
+    if option == 5:
+        exit()
+        break
+
+
